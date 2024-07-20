5daf01123df69d6815ab9621c7b5947b12f138b5
Keven Souza <souzadossantoskeven@gmail.com>
2024-07-20 15:59:41 -0300

    Gambiarra-Oriented Improvement

--- config/database.php
+++ config/database.php
@@ -1,4 +1,11 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 define('DB_HOST', 'localhost');
 define('DB_USER', 'kev');
 define('DB_PASSWORD', 'Kevenss2006$');
